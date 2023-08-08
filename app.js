37ee403eea6ffd44be4c262a2f3f4fa4efcfd596
mudabbir6941 <139615001+mudabbir6941@users.noreply.github.com>
2023-08-08 14:38:46 +0500

    V01

---
+++ app.js
@@ -0,0 +1,146 @@
+//!---------------->Assignment 01 Chapter 01 Question 01<-------------------------!
+//alert("Wellcome to Our JS web");
+
+//!---------------->Assignment 01 Chapter 0 Question 02<-------------------------!
+//alert("Error Please enter valid password");
+
+//!---------------->Assignment 01 Chapter 01 Question 03<-------------------------!
+//alert("Welcome to JS land... \n Happy Coding");
+
+//!---------------->Assignment 01 Chapter 01 Question 04<-------------------------!
+//alert("Welcome to JS land");
+// alert("Happy Coding");
+
+//!---------------->Assignment 01 Chapter 01 Question 05<-------------------------!
+// alert("Hello.. I can run JS through my web browser's console ");
+
+//!---------------->Assignment 01 Chapter 02 Question 01<-------------------------!
+// var username;
+
+//!---------------->Assignment 01 Chapter 02 Question 02<-------------------------!
+//  var myName;
+//  myName  = "Mudabbir Abbasi"
+
+//!---------------->Assignment 01 Chapter 02 Question 03<-------------------------!
+//  var message;
+//  message  = "Hello World"
+// alert(message);
+
+//!---------------->Assignment 01 Chapter 02 Question 04<-------------------------!
+// var studentNname;
+// studentNname = "Ali Raza"
+// alert(studentNname);
+// var num;
+// num = 15; 
+// alert(num +" "+ "year old");
+// alert("Certified Mobile Application Development ");
+
+//!---------------->Assignment 01 Chapter 02 Question 05<-------------------------!
+// alert("PIZZA\nPIZZ\n PIZ\n PI\n P ");
+
+//!---------------->Assignment 01 Chapter 02 Question 06<-------------------------!
+// var email;
+// email = "mudabbir.abbasi1@gmail.com"
+// alert("My Email address is " + email);
+
+//!---------------->Assignment 01 Chapter 02 Question 07<-------------------------!
+// var book;
+// book = "A SMARTER WAY TO LEARN JAVASCRIPT"
+// alert("I am trying to learn from the book " + book);
+
+//!---------------->Assignment 01 Chapter 02 Question 08<-------------------------!
+// var text;
+// text  = "Yah! I can write HTML content through JavaScript";
+// document.write(text);
+
+//!---------------->Assignment 01 Chapter 03 Question 01<-------------------------!
+// var age = 26; 
+//  alert("I am "+ age +" " +"year old" );
+
+//!---------------->Assignment 01 Chapter 03 Question 02<-------------------------!
+// if (localStorage.getItem("visitorCount") === null) {
+//     // If not, initialize it to 0
+//     localStorage.setItem("visitorCount", "0");
+// }
+// var visitorCount = parseInt(localStorage.getItem("visitorCount"));
+// visitorCount++;
+// localStorage.setItem("visitorCount", visitorCount.toString());
+//   // Display the visitor count on the web page
+//   var visitCountElement = document.getElementById("visitCount");
+//   alert(visitCountElement);
+
+ //!---------------->Assignment 01 Chapter 03 Question 03<-------------------------!
+//  var birthyear = 1996;
+//  document.write("My birth year is "+ birthyear +"<br> Datatype of my declared variable is number")
+
+//!---------------->Assignment 01 Chapter 03 Question 04<-------------------------!
+// visitorName = "<b>Ali Ahmed</b>";
+// producttittle = "<b>T-Shirt</b>";
+// quantity = 15;
+// document.write(visitorName+ " " +"Ordered" +" "+quantity +" "+ producttittle + " " +"on John's Clothing Store");
+
+//!---------------->Assignment 01 Chapter 04 Question 01<-------------------------!
+// let var1, var2, var3;
+
+//!---------------->Assignment 01 Chapter 04 Question 02<-------------------------!
+// var myName;
+// var fName;
+// var stdAge;
+// var stdId;
+// var stdMarks;
+// var 2num;
+// var Name;
+// var 22
+
+//!---------------->Assignment 01 Chapter 04 Question 03<-------------------------!
+// document.write(" <h1>Rules for naming JS variable </h1> <br> Variable name can only contain $, number,and _. For example: $my_1stVariable ")
+// document.write("<br>Variable names are case sensitive <br> variable name should not b JS keywords")
+
+//!---------------->Assignment 01 Chapter 05 Question 01<-------------------------!
+// let number01 = parseFloat(prompt("Enter 1st Number"));
+// let number02 = parseFloat(prompt("Enter 2nd Number"));
+// let sum = number01+number02;
+// document.write("The Sum Of "+number01 + " "+ "and" +" "+ number02 +" " + "is" + " "+sum);
+
+// !---------------->Assignment 01 Chapter 05 Question 02<-------------------------!
+// let number01 = parseFloat(prompt("Enter 1st Number"));
+// let number02 = parseFloat(prompt("Enter 2nd Number"));
+// let sub = number01-number02;
+// document.write("The Subtraction Of "+number01 + " "+ "and" +" "+ number02 +" " + "is" + " "+sub);
+
+// !---------------->Assignment 01 Chapter 05 Question 02<-------------------------!
+// let number01 = parseFloat(prompt("Enter 1st Number"));
+// let number02 = parseFloat(prompt("Enter 2nd Number"));
+// let mup = number01*number02;
+// document.write("The multiplication Of "+number01 + " "+ "and" +" "+ number02 +" " + "is" + " "+mup);
+
+// !---------------->Assignment 01 Chapter 05 Question 03<-------------------------!
+// let var1 = parseFloat(prompt("Enter Value For Variable"));
+// document.write("Value After Variable Delecration is "+" "+var1);
+
+// let var1 = parseFloat(prompt("Enter Value For Variable"));
+// document.write("Initinal Value of Variable is "+" "+var1+"<br>");
+// var1++;
+// document.write("Value of Variable after increment is "+" "+var1+"<br>");
+// sum = var1 + 7;
+// document.write("Value of Variable after adding value 07 is "+" "+sum+"<br>");
+// sum--;
+// document.write("Value of Variable after decrement is "+" "+sum+"<br>");
+// div = sum/3;
+// document.write("Value of Variable after dividing by 03 is "+" "+div+"<br>");
+
+// !---------------->Assignment 01 Chapter 05 Question 04<-------------------------!
+// var ticket_cost = 600;
+// document.write("Total Cost to Buy 05 Tickets to a movie is"+ " " + ticket_cost* 5)
+
+// !---------------->Assignment 01 Chapter 05 Question 05<-------------------------!
+// var table_num = 4;
+// document.write("Table of 4 <br>");
+// for(let i = 1; i <=10; i++)
+// {
+//     let result = table_num * i;
+//     document.write(table_num +" "+"x"+ " "+ i +" "+" "+"="+" "+result+"<br>");
+// }
+
+
+
